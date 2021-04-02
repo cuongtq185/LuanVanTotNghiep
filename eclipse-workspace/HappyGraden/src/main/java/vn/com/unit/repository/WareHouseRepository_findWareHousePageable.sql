da883941515e4f5069479291d678d995c0db048a
cuongtq185 <63025883+cuongtq185@users.noreply.github.com>
2021-04-02 15:30:27 +0700

    warehouse

--- eclipse-workspace/HappyGraden/src/main/java/vn/com/unit/repository/WareHouseRepository_findWareHousePageable.sql
+++ eclipse-workspace/HappyGraden/src/main/java/vn/com/unit/repository/WareHouseRepository_findWareHousePageable.sql
@@ -1,6 +1,7 @@
-SELECT *, pr.product_quantity
+SELECT *, product_id AS product, pr.product_quantity
 FROM product
 left join warehouse pr
 on product_id = product
+where product_disable = 0
 order by product_id
 OFFSET  /*offset*/ ROWS FETCH NEXT  /*sizeOfPage*/ ROWS ONLY
