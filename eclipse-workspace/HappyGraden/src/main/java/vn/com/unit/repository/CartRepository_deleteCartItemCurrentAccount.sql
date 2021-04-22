14c36dd17150b28bd627efedbebf93e2eecac40c
cuongtq <cuongtq@unit.com.vn>
2021-04-22 07:44:35 +0700

    cm

--- eclipse-workspace/HappyGraden/src/main/java/vn/com/unit/repository/CartRepository_deleteCartItemCurrentAccount.sql
+++ eclipse-workspace/HappyGraden/src/main/java/vn/com/unit/repository/CartRepository_deleteCartItemCurrentAccount.sql
@@ -1,1 +1,8 @@
-delete from p2p_cart where account = /*account_id*/ and product = /*product_id*/;
+declare @i int = (select cart_id from cart
+left join cart_item on cart_item.cart = cart.cart_id
+where cart.account = /*account_id*/'' and cart_item.product = /*product_id*/'')
+
+delete from cart_item
+where cart = @i
+delete from cart
+where cart_id = @i
