748a557e7ebe65f95f8b1190d2f899bde875f718
cuongtq <cuongtq@unit.com.vn>
2021-04-25 11:31:45 +0700

    Update ProductRepository_findProductByName.sql

--- eclipse-workspace/HappyGraden/src/main/java/vn/com/unit/repository/ProductRepository_findProductByName.sql
+++ eclipse-workspace/HappyGraden/src/main/java/vn/com/unit/repository/ProductRepository_findProductByName.sql
@@ -2,7 +2,7 @@ select product.*, p.product_price, img.product_img
 from product
 left join product_img2D as img on product.product_id = img.id_product_img2D
 left join product_price as p on product.product_id = p.product_price_id
-where 1=1 
+where product_disable = 0
 	/*IF name != '' and name != null*/
 	 and product_name like CONCAT('%',/*name*/'','%')
 	/*END*/
